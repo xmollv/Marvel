3fd89e4fc622e5e4041fe831bf8812927bb70518
Xavi Moll <xmollv@gmail.com>
2017-05-26 20:34:49 +0200

    Created the subclass of UITabBarController that will be the naviagtion root of the entire app

---
+++ Marvel/Marvel/TabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarViewController.swift
+//  Marvel
+//
+//  Created by Xavi Moll on 26/05/2017.
+//  Copyright © 2017 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
