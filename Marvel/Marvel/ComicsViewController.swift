ade65f4171f0e61bda4621d3d76a4a190f761b90
Xavi Moll <xmollv@gmail.com>
2017-05-27 12:42:08 +0200

    Added the collectionView to display the list of comics

--- Marvel/Marvel/ComicsViewController.swift
+++ Marvel/Marvel/ComicsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ComicsViewController: UIViewController {
     
+    @IBOutlet var collectionView: UICollectionView!
+    
     var dataProvider: DataProvider!
 
     override func viewDidLoad() {
@@ -22,8 +24,54 @@ class ComicsViewController: UIViewController {
     }
     
     private func configureViewController() {
+        automaticallyAdjustsScrollViewInsets = false
         view.backgroundColor = Stylesheet.Color.grey1
         title = "Comics"
+        customizeCollectionView()
+    }
+    
+    private func customizeCollectionView() {
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.contentInset = UIEdgeInsetsMake(10, 10, 10, 10)
     }
 
+}
+
+extension ComicsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ComicsCell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+}
+
+extension ComicsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
+            fatalError("The layout for the collection view was not a UICollectionViewFlowLayout")
+        }
+        
+        let cellsForRow: CGFloat = 2.0 // This could be customized by the user or when the app is in landscape/iPad
+        let cellSpacing = flowLayout.minimumInteritemSpacing
+        let collectionViewInsetsWidth = collectionView.contentInset.left + collectionView.contentInset.right
+        
+        let collectionViewWidthMinusInsets = collectionView.bounds.size.width - collectionViewInsetsWidth
+        let collectionViewWidthMinusCellSpacing = collectionViewWidthMinusInsets - (cellSpacing * (cellsForRow - 1))
+        let widthCell = collectionViewWidthMinusCellSpacing / cellsForRow
+        
+        let sizeToReturn = CGSize(width: widthCell, height: 200)
+        return sizeToReturn
+    }
+}
+
+extension ComicsViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        Logger.log(message: "Tapped: \(indexPath.row)", event: .debug)
+    }
 }
